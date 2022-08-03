c58fa1bf5e69b1ced8e21dd376a52589b2c6ab38
Brandon Flores <brandonflores647@gmail.com>
2022-08-03 13:11:38 -0400

    added create map route

--- app/api/map_routes.py
+++ app/api/map_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required
-from app.models import Map
+from app.models import db, Map
 
 map_routes = Blueprint('map', __name__)
 
@@ -9,3 +9,14 @@ def getAll(userId):
     maps = Map.query.filter_by(ownerId=userId).all()
     data = [i.toDict() for i in maps]
     return {'maps': data}
+
+@map_routes.route('/new', methods=['POST'])
+def create():
+    data = request.json
+    new_map = Map(
+        name=data['name'],
+        ownerId=data['ownerId']
+    )
+    db.session.add(new_map)
+    db.session.commit()
+    return new_map.toDict()
