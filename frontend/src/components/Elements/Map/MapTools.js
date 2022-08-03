7e7bfa9a470c6f32cd5233b2384cae2948990236
Brandon Flores <brandonflores647@gmail.com>
2022-08-03 16:47:55 -0400

    now handling basic create thunk

--- frontend/src/components/Elements/Map/MapTools.js
+++ frontend/src/components/Elements/Map/MapTools.js
@@ -1,3 +1,6 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { thunkCreateMap } from "../../../store/maps";
+
 const MapTools = ({
     grid,
     playing,
@@ -6,9 +9,21 @@ const MapTools = ({
     handlePlay,
     handleClear }) => {
 
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.session.user);
+
     const doClear = () => {
         handleClear(grid)
         setClear(!clear)
+    }
+
+    const handleSave = () => {
+        (async () => {
+            await dispatch(thunkCreateMap({
+                name: 'New Map',
+                ownerId: user.id
+            }))
+        })();
     }
 
     return (
@@ -21,6 +36,10 @@ const MapTools = ({
                 disabled={playing}
                 onClick={() => doClear()}
             >CLEAR</button>
+            <button
+                disabled={playing}
+                onClick={() => handleSave()}
+            >SAVE</button>
         </div>
     );
 }
