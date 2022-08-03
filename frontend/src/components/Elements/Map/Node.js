f97a0c489d848b202244b420131707fa60caed94
Brandon Flores <brandonflores647@gmail.com>
2022-08-03 00:54:46 -0400

    users can no longer draw while animation is playing

--- frontend/src/components/Elements/Map/Node.js
+++ frontend/src/components/Elements/Map/Node.js
@@ -2,21 +2,20 @@ import { useState, useEffect, useRef } from 'react';
 
 import classes from './Node.module.css';
 
-const Node = ({ grid, setGrid, row, col, isStart, isEnd, isWall }) => {
+const Node = ({ grid, setGrid, playing, row, col, isStart, isEnd, isWall }) => {
     const [wallVisual, setWallVisual] = useState(isWall);
-
     const ref = useRef(null);
 
     useEffect(() => {
         const element = ref.current;
         const handleClick = (e) => {
-            if (e.buttons===1 && !isStart && !isEnd) {
+            if (e.buttons===1 && !isStart && !isEnd && !playing) {
                 setWallVisual(true);
                 const updatedGrid = grid.slice();
                 updatedGrid[row][col].isWall = true;
                 setGrid(updatedGrid);
             }
-            if (e.buttons===2 && !isStart && !isEnd) {
+            if (e.buttons===2 && !isStart && !isEnd && !playing) {
                 setWallVisual(false);
                 const updatedGrid = grid.slice();
                 updatedGrid[row][col].isWall = false;
@@ -33,7 +32,7 @@ const Node = ({ grid, setGrid, row, col, isStart, isEnd, isWall }) => {
             element.removeEventListener('mouseover', handleClick);
             document.removeEventListener('contextmenu', stopMenu);
         }
-    }, []);
+    }, [playing]);
 
     return (
         <span id={`node-${row}-${col}`} className={`
--- frontend/src/components/Elements/Map/index.js
+++ frontend/src/components/Elements/Map/index.js
@@ -17,6 +17,7 @@ const Map = () => {
     const speed = 10;
 
     const [grid, setGrid] = useState([]);
+    const [playing, setPlaying] = useState(false);
 
     const nodeTemplate = (row, col) => {
         return {
@@ -101,6 +102,7 @@ const Map = () => {
 
             }, speed+25 * i)
         });
+        setPlaying(false);
     }
 
     const handlePlay = () => {
@@ -109,12 +111,15 @@ const Map = () => {
         const visitedNodes = dijkstras(grid, startNode, endNode);
         const pathArr = getPath(endNode);
         animateVisited(visitedNodes, pathArr);
-        console.log(grid)
+        setPlaying(true);
     }
 
     return (
-        <div className={classes.gridContainer}>
-            <button onClick={() => handlePlay()}>PLAY</button>
+        <div className={classes.gridContainer} style={{cursor:(playing?'not-allowed':'pointer')}}>
+            <button
+                disabled={playing}
+                onClick={() => handlePlay()}
+            >PLAY</button>
             {grid.map((row, i) => (
                 <div className={classes.rowContainer} key={`row-${i}`}>
                     {row.map((node, i) => (
@@ -122,6 +127,7 @@ const Map = () => {
                             key={`node-${i}`}
                             grid={grid}
                             setGrid={setGrid}
+                            playing={playing}
                             row={node.row}
                             col={node.col}
                             isStart={node.isStart}
