68fb2ca035f54590cf939655f12a843e12695144
Brandon Flores <brandonflores647@gmail.com>
2022-08-04 12:17:58 -0400

    moved getAllMaps thunk to MapPage, now properly loading maps upon login

--- frontend/src/components/Pages/MapPage/index.js
+++ frontend/src/components/Pages/MapPage/index.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import Map from "../../Elements/Map";
 import TemplateList from "../../Elements/TemplateList";
 
 import classes from './MapPage.module.css';
+import { thunkGetAllMaps } from "../../../store/maps";
 
 const MapPage = () => {
+    const user = useSelector((state) => state.session.user);
+    const [loaded, setLoaded] = useState(false);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        (async() => {
+            if (loaded && user) {
+                await dispatch(thunkGetAllMaps(user.id));
+            }
+        })();
+        setLoaded(true);
+    }, [dispatch, loaded]);
+
     return (
         <div className={classes.pageWrapper}>
             <Map />
