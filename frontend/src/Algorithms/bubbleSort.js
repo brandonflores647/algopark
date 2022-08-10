0a0aea53f13720fc1b5b26b704444ff890095d8e
Brandon Flores <brandonflores647@gmail.com>
2022-08-09 20:03:09 -0400

    sortStacks function is now dynamic, can accept any algorithm if formatted correctly

--- frontend/src/Algorithms/bubbleSort.js
+++ frontend/src/Algorithms/bubbleSort.js
@@ -1,22 +1,43 @@
-const bubbleSort = (arr) => {
+import classes from '../components/Pages/SortingPage/SortingPage.module.css';
+
+const bubbleSort = async (arr, speed) => {
+    const newOrder = arr.slice();
+    const timer = ms => new Promise(res => setTimeout(res, ms));
+
     let swapped = false;
+    for (let i = 0; i < newOrder.length-1; i++) {
+      swapped = false;
 
-    for (let i = 0; i < arr.length-1; i++) {
-        swapped = false;
+      for (let j = 0; j < newOrder.length-1; j++) {
+        const curEle = document.getElementById(
+          `stack-${j}`
+        );
+        const nextEle = document.getElementById(
+          `stack-${j+1}`
+        );
 
-        for (let j = 0; j < arr.length-1; j++) {
-            if (arr[j].height > arr[j+1].height) {
-                const prev = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = prev;
-                swapped = true;
-            }
+        if (newOrder[j].height > newOrder[j+1].height) {
+            const prev = newOrder[j];
+
+            curEle.className = classes.selected;
+            nextEle.className = classes.selectedNext;
+            await timer(speed);
+            curEle.className = classes.selectedNext;
+            nextEle.className = classes.selected;
+            curEle.style.height = `${newOrder[j+1].height}%`;
+            nextEle.style.height = `${newOrder[j].height}%`;
+            await timer(speed);
+
+            newOrder[j] = newOrder[j+1];
+            newOrder[j+1] = prev;
+            swapped = true;
+            curEle.className = classes.stack;
+            nextEle.className = classes.stack;
         }
+      }
 
-        if (!swapped) break;
+      if (!swapped) break;
     }
-
-    return arr;
 }
 
 export default bubbleSort;
--- frontend/src/components/Pages/SortingPage/index.js
+++ frontend/src/components/Pages/SortingPage/index.js
@@ -36,44 +36,44 @@ const SortingPage = () => {
   }
 
   const sortStacks = async (algo) => {
-    // const newOrder = algo(stacks);
-    const newOrder = stacks.slice();
-    const timer = ms => new Promise(res => setTimeout(res, ms));
+    algo(stacks, speed);
+    // const newOrder = stacks.slice();
+    // const timer = ms => new Promise(res => setTimeout(res, ms));
 
-    let swapped = false;
-    for (let i = 0; i < newOrder.length-1; i++) {
-      swapped = false;
+    // let swapped = false;
+    // for (let i = 0; i < newOrder.length-1; i++) {
+    //   swapped = false;
 
-      for (let j = 0; j < newOrder.length-1; j++) {
-        const curEle = document.getElementById(
-          `stack-${j}`
-        );
-        const nextEle = document.getElementById(
-          `stack-${j+1}`
-        );
+    //   for (let j = 0; j < newOrder.length-1; j++) {
+    //     const curEle = document.getElementById(
+    //       `stack-${j}`
+    //     );
+    //     const nextEle = document.getElementById(
+    //       `stack-${j+1}`
+    //     );
 
-        if (newOrder[j].height > newOrder[j+1].height) {
-            const prev = newOrder[j];
+    //     if (newOrder[j].height > newOrder[j+1].height) {
+    //         const prev = newOrder[j];
 
-            curEle.className = classes.selected;
-            nextEle.className = classes.selectedNext;
-            await timer(speed);
-            curEle.className = classes.selectedNext;
-            nextEle.className = classes.selected;
-            curEle.style.height = `${newOrder[j+1].height}%`;
-            nextEle.style.height = `${newOrder[j].height}%`;
-            await timer(speed);
+    //         curEle.className = classes.selected;
+    //         nextEle.className = classes.selectedNext;
+    //         await timer(speed);
+    //         curEle.className = classes.selectedNext;
+    //         nextEle.className = classes.selected;
+    //         curEle.style.height = `${newOrder[j+1].height}%`;
+    //         nextEle.style.height = `${newOrder[j].height}%`;
+    //         await timer(speed);
 
-            newOrder[j] = newOrder[j+1];
-            newOrder[j+1] = prev;
-            swapped = true;
-            curEle.className = classes.stack;
-            nextEle.className = classes.stack;
-        }
-      }
+    //         newOrder[j] = newOrder[j+1];
+    //         newOrder[j+1] = prev;
+    //         swapped = true;
+    //         curEle.className = classes.stack;
+    //         nextEle.className = classes.stack;
+    //     }
+    //   }
 
-      if (!swapped) break;
-    }
+    //   if (!swapped) break;
+    // }
   }
 
   return (
