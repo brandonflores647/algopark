957a1c8e1b355dc8e8d60e42dbbcb596d840f938
Brandon Flores <brandonflores647@gmail.com>
2022-08-11 13:20:22 -0400

    added title

---
+++ frontend/src/components/Pages/SplashPage/index.js
@@ -0,0 +1,17 @@
+import classes from './SplashPage.module.css';
+
+const SplashPage = () => {
+    return (
+        <div className={classes.splashWrapper}>
+            <div className={classes.titleContainer}>
+                <div className={classes.title}>
+                    <h1>AlgoPark helps users visualize and interact with algorithms.</h1>
+                    <span>Choose from pathfinding or sorting algorithms. Draw a path and watch dijkstras algorithm navigate through the obstacles you created. Hop into our sorting page to randomize a set of stacks and choose your speed. Watch the algorithm of your choosing sort those stacks in real time!</span>
+                </div>
+                <img className={classes.gridTop} src='/static/sortVisual1.png' alt='sort visual' width={'400px'}/>
+            </div>
+        </div>
+    );
+}
+
+export default SplashPage;
